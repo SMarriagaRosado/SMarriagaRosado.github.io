76c98b12ec53cf150603e566fdfb0ccf415f7dab
SMarriagaRosado <136289405+SMarriagaRosado@users.noreply.github.com>
2023-07-11 20:14:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function abrirCarta() {
   var carta = document.querySelector('.carta');
   carta.classList.toggle('abierta');
+
+  var musica = document.getElementById('musica');
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
 }
+
